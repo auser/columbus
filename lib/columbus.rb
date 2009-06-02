7b3d2226d351ba8dfc0fd7c3d9edac1222057ec9
Ari Lerner <arilerner@mac.com>
2009-06-02 12:57:35 -0700

    Added gitstylebinaries into vendor

--- lib/columbus.rb
+++ lib/columbus.rb
@@ -1,6 +1,10 @@
 $:.unshift(File.dirname(__FILE__) + "/columbus")
 require "rubygems"
 require "dnssd"
+
+$:.unshift(File.dirname(__FILE__) + "/../vendor/gems")
+require "git-style-binaries/lib/git-style-binary"
+
 require "server"
 require "client"
 
